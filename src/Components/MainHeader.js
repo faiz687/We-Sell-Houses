0c7c3ddce989c68aa642ef85ed02a51f487007e9
faizaan chowdhary <chowdhaf@uni.coventry.ac.uk>
2020-11-10 21:35:44 +0000

    Registration Page created

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -10,8 +10,7 @@ function MainHeader(props) {
    <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
       <Menu.Item key="1" ><Link to="/">Home</Link></Menu.Item>
-      <Menu.Item key="2">About</Menu.Item>
-      <Menu.Item key="3">Contact</Menu.Item>
+      <Menu.Item key="2" ><Link to="/Register">Register</Link></Menu.Item>
    </Menu>
 </Header>
 </>
