05de077bc91b22b13823dceb2672dad7138a42b5
faizaan chowdhary <chowdhaf@uni.coventry.ac.uk>
2020-11-23 21:41:26 +0000

    Adding house Created

--- src/Components/HouseCategory.js
+++ src/Components/HouseCategory.js
@@ -2,30 +2,17 @@ import React from 'react';
 import { Select } from 'antd';
 const { Option } = Select;
 
-
 function HouseCategory(props) {
-  
-  
-  console.log("this is props")
-  console.log(props)
-  const p = props.parentMethod.OnCategoryChange()
 
-  
-
-  
-  
-                                        
   return (
-                  <Select placeholder="Select a Category" onChange={p} allowClear>
-                <Option value="Apartment">Apartment</Option>
-                <Option value="Flat">Flat</Option>
-                <Option value="Detached">Detached</Option>
-                <Option value="Bunglow">Bunglow</Option>
-                <Option value="Semi-Detached">Semi-Detached</Option>
-              </Select> 
-    
-        
-    );
+          <Select placeholder="Select a Category" onChange={props.OnChange}  allowClear>
+              <Option value="Apartment">Apartment</Option>
+              <Option value="Flat">Flat</Option>
+              <Option value="Detached">Detached</Option>
+              <Option value="Bunglow">Bunglow</Option>
+              <Option value="Semi-Detached">Semi-Detached</Option>
+          </Select> 
+          );
 }
 
 export default HouseCategory;
